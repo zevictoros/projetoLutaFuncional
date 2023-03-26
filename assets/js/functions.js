4020a6fbd71184247cd46fbe3967432c1b0358e6
José Victor <zevictoros1808@gmail.com>
2023-03-26 10:35:28 -0300

    Criando cenário

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -49,3 +49,37 @@ const createBigMonster = () => {
         defense: 6
     }
 }
+
+const stage = {
+    fighter1: null,
+    fighter2: null,
+    fighter1El: null,
+    fighter2El: null,
+    start(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+    
+        this.update();
+    },
+    update(){
+        // Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
+
+        //Fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
+    },
+    doAttack(attacking, attacked){
+        console.log(`${attacking.name} atacando ${attacked.name}`)
+
+        this.update();
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 const char = createKnight('Victor');
 const monster = createLittleMonster();
 
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+);
